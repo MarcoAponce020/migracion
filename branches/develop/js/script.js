1a19e6959803bf6bade09b69739061cc9d9640dd
cruzjair <cruzjair@67cbbcac-a3c6-4ebf-a414-c2be0664298b>
2024-07-18 20:04:55 +0000

    svn commit -m "se movio el contenido actual a trunk"
    git-svn-id: https://svn.code.sf.net/p/proyecto-demo-svn/svn@16 67cbbcac-a3c6-4ebf-a414-c2be0664298b

--- branches/develop/js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("clickMeButton").addEventListener("click", function () {
-  var textInput = document.getElementById("textInput").value;
-  var message = document.getElementById("message");
-  if (textInput) {
-    message.textContent = "¡Has escrito: " + textInput + "!";
-  } else {
-    message.textContent = "¡Botón clickeado!";
-  }
-});
